8855789a6e44b28d95b43d8cb1149114b62681bb
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-11-21 22:15:47 +0300

    add blocks finder

--- app.rb
+++ app.rb
@@ -3,7 +3,20 @@ def main(args)
   filename = args.first
   file = File.open(filename, 'rb')
   content = file.read
-  p content
+  [1, 2, 3].each do |n|
+    blocks = []
+    size = content.size
+    regex = '.' * n
+    p "block size is: #{n}"
+    content.scan(Regexp.new(regex)) do |b|
+      blocks << {block: b,
+                 count: content.chars.each_cons(b.size).map(&:join).count(b),
+                 x: content.chars.each_cons(b.size).map(&:join).count(b).to_f / size}
+      p b
+      p "count: #{blocks.last[:count]} / #{size}"
+      p "x: #{blocks.last[:count]}"
+    end
+  end
 end
 
 main(ARGV)
