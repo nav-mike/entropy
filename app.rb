193e8562b8cc6b5d02b0db93b14fd398554fc84f
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-11-22 15:17:28 +0300

    add algorithm 1

--- app.rb
+++ app.rb
@@ -3,19 +3,46 @@ def main(args)
   filename = args.first
   file = File.open(filename, 'rb')
   content = file.read
-  [1, 2, 3].each do |n|
+  ns = [1, 2, 3]
+  file = File.open('result.txt', 'w')
+  ns.each do |n|
     blocks = []
+    string_blocks = []
     size = content.size
-    regex = '.' * n
-    p "block size is: #{n}"
+    regex = '[a-zA-Z0-9]' * n
+    file.write "block size is: #{n}\n"
+    index = 0
+    bsize = content.scan(Regexp.new(regex)).count
     content.scan(Regexp.new(regex)) do |b|
-      blocks << {block: b,
-                 count: content.chars.each_cons(b.size).map(&:join).count(b),
-                 x: content.chars.each_cons(b.size).map(&:join).count(b).to_f / size}
-      p b
-      p "count: #{blocks.last[:count]} / #{size}"
-      p "x: #{blocks.last[:count]}"
+      unless string_blocks.include? b
+        block = {block: b,
+                   count: content.chars.each_cons(b.size).map(&:join).count(b),
+                   x: content.chars.each_cons(b.size).map(&:join).count(b).to_f / size}
+
+        # calculate fields
+        block[:p] = block[:x].to_f / (size - n + 1)
+
+        blocks << block
+        string_blocks << b
+        # print
+        file.write "index: #{index += 1}/#{bsize}\n"
+        file.write "#{b}\n"
+        file.write "count: #{block[:count]} / #{size}\n"
+        file.write "x: #{block[:count]}\n"
+        file.write "p: #{block[:p]}\n"
+        file.write "=================================\n"
+      end
     end
+
+    sum = 0.0
+    blocks.each do |b|
+      sum += b[:p] * Math::log(b[:p], 2)
+    end
+    sum = (-1 * sum) / n
+    file.write "++++++++++++++++++++++++++++++++++\n"
+    p "sum: #{sum}"
+    file.write "sum: #{sum}\n"
+    file.write "##################################\n"
   end
 end
 
